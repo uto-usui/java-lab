118bc7ff569b1cff3c4e545e084ba02edf6ad483
uto-usui <me@uto-usui.com>
2024-06-05 20:30:39 +0900

    Handle no arguments and incorrect number of arguments in Lec14Main

--- src/teikyo/ics/csp1lec14/Lec14Main.java
+++ src/teikyo/ics/csp1lec14/Lec14Main.java
@@ -5,16 +5,46 @@ import teikyo.ics.csp1lec14.student.*;
 
 public class Lec14Main {
     public static void main(String[] args) {
-        Teacher t = new Teacher();
-        t.setValues("Taro", 40);
+        try {
+            if (args.length == 0 || args.length % 3 != 0) {
+                throw new IllegalArgumentException("プログラム引数を見直してください");
+            }
 
-        Student s = new Student();
-        s.setValues("Hanako", 20);
+            Person[] persons = new Person[args.length / 3];
+            int index = 0;
 
-        Person[] persons = {t, s};
+            for (int i = 0; i < args.length; i += 3) {
+                String type = args[i];
+                String name = args[i + 1];
+                int age;
 
-        for (int i = 0; i < persons.length; i++) {
-            persons[i].printInfo();
+                try {
+                    age = Integer.parseInt(args[i + 2]);
+                } catch (NumberFormatException e) {
+                    System.out.println("年齢が正しく入力されていません");
+                    return;
+                }
+
+                if (type.equals("Teacher")) {
+                    Teacher t = new Teacher();
+                    t.setValues(name, age);
+                    persons[index++] = t;
+                } else if (type.equals("Student")) {
+                    Student s = new Student();
+                    s.setValues(name, age);
+                    persons[index++] = s;
+                } else {
+                    System.out.println("不正なクラス指定です: " + type);
+                    return;
+                }
+            }
+
+            for (Person person : persons) {
+                person.printInfo();
+            }
+
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
+            System.out.println("プログラム引数を見直してください");
         }
     }
 }
